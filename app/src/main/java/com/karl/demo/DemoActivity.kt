ba71063890f4d35593a370f6ad5e7494d9cdc70c
辽东小小 <liaodongxiaoxiao@gmail.com>
2021-10-14 18:05:03 +0800

    add menu to demo main page

--- app/src/main/java/com/karl/demo/DemoActivity.kt
+++ app/src/main/java/com/karl/demo/DemoActivity.kt
@@ -2,9 +2,7 @@ package com.karl.demo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.karl.demo.demo.BackKeyDemoActivity
-import com.karl.demo.demo.MQTTDemoActivity
-import com.karl.demo.demo.ScrollViewDemoActivity
+import com.karl.demo.demo.*
 import com.karl.demo.extesion.justStartActivity
 import kotlinx.android.synthetic.main.activity_demo.*
 
@@ -23,6 +21,38 @@ class DemoActivity : AppCompatActivity() {
 
         btn_demo_mqtt.setOnClickListener {
             justStartActivity(MQTTDemoActivity::class.java)
+        }
+
+        btn_demo_html.setOnClickListener {
+            justStartActivity(HtmlDemoActivity::class.java)
+        }
+
+        btn_okhttp_util.setOnClickListener {
+            justStartActivity(OkHttpUtilsDemoActivity::class.java)
+        }
+
+        btn_Scrollview_demo.setOnClickListener {
+            justStartActivity(ScrollViewDemoActivity::class.java)
+        }
+
+        btn_Scrollview_bottom.setOnClickListener {
+            justStartActivity(ScrollViewListenerDemoActivity::class.java)
+        }
+
+        btn_view_to_image.setOnClickListener {
+            justStartActivity(ViewToImageActivity::class.java)
+        }
+
+        btn_to_do.setOnClickListener {
+            justStartActivity(LoginActivity::class.java)
+        }
+
+        btn_weather.setOnClickListener {
+            justStartActivity(WeatherDemoActivity::class.java)
+        }
+
+        btn_lottery.setOnClickListener {
+            justStartActivity(LotteryDemoActivity::class.java)
         }
 
     }
